aadaa63dc29a22383edd72df35c038f429346491
solumon <solumon@sina.com>
2016-06-13 09:45:35 +0800

    Initial Commit

---
+++ ZLKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLKitDemo
+//
+//  Created by iLuffy  on 16/6/13.
+//  Copyright © 2016年 ZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
